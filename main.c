bb74c794c8b7e5596e8afdf92592b2a231f6ced2
YashB11 <bichewar.y@northeastern.edu>
2022-04-21 02:11:59 -0400

    main file added

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * Author: Embedded IoT Spring 22 batch
+ * Purpose: To demonstrate face detection and actuation.
+ * Language:  C
+ */
+
+
+
+#include <stdio.h>
+
+
+/* Project starts here */
+int main(void)
+{
+    
+    printf("PROJECT STARTS HERE !! ");
+
+    //ADD RESPECTIVE HEDER FILES AND FUNCTION CALLS BELOW
+    
+    return 0;
+}
